db356c16610002513525a660150f87e56513626e
George Markosian <georgemarkosian@gmail.com>
2022-09-03 14:28:18 +0200

    Simple UI layers component added.

---
+++ Modules/UI/UILayersComponent.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using Build1.PostMVC.Core.MVCS.Injection;
+using Build1.PostMVC.Unity.App.Mediation;
+using UnityEngine;
+
+namespace Build1.PostMVC.Unity.App.Modules.UI
+{
+    [DisallowMultipleComponent]
+    public sealed class UILayersComponent : UnityView
+    {
+        [SerializeField] private List<UILayerInfo> layers;
+
+        [Inject] public IUILayersController UILayersController { get; set; }
+
+        [PostConstruct]
+        public void PostConstruct()
+        {
+            foreach (var layer in layers)
+                UILayersController.RegisterLayer(layer.Id, layer.GameObject);
+        }
+    }
+}
