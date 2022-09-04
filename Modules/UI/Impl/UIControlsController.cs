67e6eb83aac82262ecf64c529fda04778610bf89
George Markosian <georgemarkosian@gmail.com>
2022-09-04 19:15:49 +0200

    Minor code optimization.

--- Modules/UI/Impl/UIControlsController.cs
+++ Modules/UI/Impl/UIControlsController.cs
@@ -164,11 +164,10 @@ namespace Build1.PostMVC.Unity.App.Modules.UI.Impl
         {
             foreach (var binding in configuration)
             {
-                var bindingTo = binding.viewInterfaceType != null
-                                    ? MediationBinder.Rebind(binding.viewType, binding.viewInterfaceType)
-                                    : MediationBinder.Rebind(binding.viewType);
-                if (binding.mediatorType != null)
-                    bindingTo.To(binding.mediatorType);
+                if (binding.viewInterfaceType != null)
+                    MediationBinder.Rebind(binding.viewType, binding.viewInterfaceType).To(binding.mediatorType);
+                else
+                    MediationBinder.Rebind(binding.viewType).To(binding.mediatorType);
             }
 
             _installedConfigs.Add(configuration);
