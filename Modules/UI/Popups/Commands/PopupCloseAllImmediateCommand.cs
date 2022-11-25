04ce475a134117c28fcbfa26d4910503081ce493
George Markosian <georgemarkosian@gmail.com>
2022-11-25 17:07:29 +0100

    Popups commands added.

---
+++ Modules/UI/Popups/Commands/PopupCloseAllImmediateCommand.cs
@@ -0,0 +1,16 @@
+using Build1.PostMVC.Core.MVCS.Commands;
+using Build1.PostMVC.Core.MVCS.Injection;
+
+namespace Build1.PostMVC.Unity.App.Modules.UI.Popups.Commands
+{
+    [Poolable]
+    public sealed class PopupCloseAllImmediateCommand : Command
+    {
+        [Inject] public IPopupController PopupController { get; set; }
+        
+        public override void Execute()
+        {
+            PopupController.CloseAll(true);
+        }
+    }
+}
