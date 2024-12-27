28fadcf9f660ad951cde213f8ae76d65c9b1ca69
George Markosian <georgemarkosian@gmail.com>
2024-12-27 20:04:24 +0800

    InternetReachabilityController improvements.

--- Modules/InternetReachability/IInternetReachabilityController.cs
+++ Modules/InternetReachability/IInternetReachabilityController.cs
@@ -6,8 +6,10 @@ namespace Build1.PostMVC.Unity.App.Modules.InternetReachability
     {
         bool LastResult     { get; }
         long LastResultCode { get; }
-
-        void Check(Action<bool> onComplete, int timeout);
+        
         void Check(Action<bool> onComplete);
+        void Check(Action<bool> onComplete, int timeout);
+        void Check(string url, Action<bool> onComplete);
+        void Check(string url, Action<bool> onComplete, int timeout);
     }
 }
