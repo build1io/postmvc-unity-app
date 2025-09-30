8e07474691ee8f756cc1405bfe2c37c6f46556b0
George Markosian <georgemarkosian@gmail.com>
2025-09-30 17:37:35 +0800

    Logs API improvements. Read methods with lines limit added.

--- Modules/Logging/LogFile.cs
+++ Modules/Logging/LogFile.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace Build1.PostMVC.Unity.App.Modules.Logging
@@ -21,9 +23,47 @@ namespace Build1.PostMVC.Unity.App.Modules.Logging
             return Path.GetFileName(path);
         }
 
-        public string ReadContent()
+        public string ReadAll()
         {
             return File.ReadAllText(path);
+        }
+
+        public string ReadLines(int lineLimit, bool addLimitMessage)
+        {
+            return ReadLines(lineLimit, addLimitMessage, out _);
+        }
+        
+        public string ReadLines(int lineLimit, out bool exceedsLimit)
+        {
+            return ReadLines(lineLimit, false, out exceedsLimit);
+        }
+
+        public string ReadLines(int lineLimit, bool addLimitMessage, out bool exceedsLimit)
+        {
+            exceedsLimit = false;
+            
+            var queue = new Queue<string>(lineLimit);
+            using (var reader = new StreamReader(path))
+            {
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    if (queue.Count == lineLimit)
+                    {
+                        queue.Dequeue();
+                        exceedsLimit = true;
+                    }
+                        
+                    queue.Enqueue(line);
+                }
+            }
+            
+            var log = string.Join(Environment.NewLine, queue);
+
+            if (exceedsLimit && addLimitMessage)
+                log = $"========== TRIMMED TO LIMIT ==========\n\n{log}";
+
+            return log;
         }
     }
 }
